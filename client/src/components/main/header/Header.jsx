a7a2f76b33747142ed2d6fdd68a53f2b970b9f53
DESKTOP-8AG16VG\TJ <iamonlyone.u1@gmail.com>
2023-11-14 15:13:01 +0900

    2023-11-14 헤더 ing 하나

--- client/src/components/main/header/Header.jsx
+++ client/src/components/main/header/Header.jsx
@@ -1,31 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FiChevronDown } from "react-icons/fi";
+import { FiChevronUp } from "react-icons/fi";
+import ShowMenu from "./ShowMenu";
+import HeaderNavPopularView from "./HeaderNavPopularView";
 
 export default function Header() {
 
-  let [showMenu, setShowMenu] = useState(false)
+  let [hovering, setHovering] = useState(1)
+  let [showMenu, setShowMenu] = useState(1)
 
-  function showNavMenu() {
-    if (showMenu === true) {
-      setShowMenu(false)
+  useEffect(() => {
+    switch (hovering) {
+      case 1:
+        setShowMenu(1);
+        break;
+      case 2:
+        setShowMenu(2);
+        break;
+      case 3:
+        setShowMenu(3);
+        break;
+      default:
+        setShowMenu(1);
+    }
+  }, [hovering])
+
+  function hoverMenu(checkFlag, event) {
+    if (checkFlag === 1) {
+      setHovering(1);
+    } else if (checkFlag === 2) {
+      setHovering(2);
     } else {
-      setShowMenu(true)
+      setHovering(3);
     }
   }
 
-  const handleMouseLeave = () => {
-    if (showMenu === true) {
-      setShowMenu(false)
+  function handleMouseLeave() {
+    if (showMenu === 2 || showMenu === 3) {
+      setShowMenu(1);
     }
-    // } else {
-    //   setShowMenu(true)
-    // }
   }
 
   return (
     <>
-      <header className="main_header">
+      <header className="main_header" onMouseLeave={handleMouseLeave}>
         <div className="main_header_layout inner">
           <div className="header_logo">
             <div className="header_logoBox">
@@ -34,13 +53,13 @@ export default function Header() {
             <div className="header_logo_left">
               <ul className="header_logo_left_ul">
                 <li className="header_logo_left_ul_li">
-                  <Link to="/">커뮤니티</Link>
-                </li>
-                <li className="header_logo_left_ul_li" >
-                  <Link to="/store" onMouseOver={showNavMenu} >쇼핑</Link>
+                  <Link to="/" onMouseEnter={() => { hoverMenu(1) }}>커뮤니티</Link>
                 </li>
                 <li className="header_logo_left_ul_li">
-                  <Link to="/experts">이사/시공/생활</Link>
+                  <Link to="/store" onMouseEnter={() => { hoverMenu(2) }}>쇼핑</Link>
+                </li>
+                <li className="header_logo_left_ul_li">
+                  <Link to="/experts" onMouseEnter={() => { hoverMenu(3) }}>이사/시공/생활</Link>
                 </li>
               </ul>
             </div>
@@ -53,28 +72,18 @@ export default function Header() {
               <Link to="/login" className="header_logo_menu">로그인</Link>
               <Link to="/normal_users/new" className="header_logo_menu">회원가입</Link>
               <Link to="/customer_center" className="header_logo_menu">고객센터</Link>
-              {<button>글쓰기<FiChevronDown /></button>}
+              <button>글쓰기<FiChevronDown /></button>
             </div>
           </div>
-          <div className="header_nav">
-            <div className="header_nav_menu">
-              {showMenu === true ?
-                <div onMouseLeave={handleMouseLeave}>
-                  <div className="header_nav_menu_popup">
-                    <Link to="/"><p>쇼핑홈</p></Link>
-                    <Link to="/"><p>카테고리</p></Link>
-                  </div>
-                </div>
-                :
-                <>
-                  <Link to="/"><p>홈</p></Link>
-                  <Link to="/"><p>추천</p></Link>
-                </>
-              }
-            </div>
+        </div>
+        <div className="main_header_layout_down">
+          <div className="header_nav  inner">
+            {showMenu === 1 && <ShowMenu menuName="community" />}
+            {showMenu === 2 && <ShowMenu menuName="store" />}
+            {showMenu === 3 && <ShowMenu menuName="experts" />}
             <div className="header_nav_popular">
               <Link to="/" className="header_nav_popular_view">
-                <div>
+                <div className="header_nav_popular_view_list">
                   <span>1</span>
                   <span><img src="images/headers/new.png" alt="" /></span>
                   <span>물티슈케이스</span>
@@ -86,20 +95,10 @@ export default function Header() {
                   <div className="header_nav_popup_title">
                     <h2>인기검색어</h2>
                     <span className="arrow_img_popup">
-                      <FiChevronDown />
+                      <FiChevronUp />
                     </span>
                   </div>
-                  <ul>
-                    <li>
-                      <Link to="/" className="header_nav_popular_view_popup">
-                        <div>
-                          <span>1</span>
-                          <span><img src="images/headers/new.png" alt="" /></span>
-                          <span>물티슈케이스</span>
-                        </div>
-                      </Link>
-                    </li>
-                  </ul>
+                  <HeaderNavPopularView />
                 </div>
               </span>
             </div>
