a74dcd83def5e8b208f8ef727f85ec2f4d8f38c0
DESKTOP-VH4UGLQ\TJ <ygs@mail.com>
2023-11-14 15:30:57 +0900

    학원 20231114 shopcate 아마도완성

--- client/src/pages/main/shopcate/ShopcateContents.jsx
+++ client/src/pages/main/shopcate/ShopcateContents.jsx
@@ -1,6 +1,24 @@
+import { useEffect, useState } from "react";
+import ShopitemContents from "../shopitem/ShopitemContents";
+import ShopcateContentsItem from "./components/contents/ShopcateContentsItem";
+
 export default function ShopcateContents(){
+  const [shopArray,setShopArray] = useState([]);
+  useEffect(()=>{
+    fetch('/db/shopbest.json')
+    .then(res=>res.json())
+    .then(data=>{setShopArray(data)})
+  },[])
   return(
     <div className="shopcate_contents">
+      {shopArray.map((list,i)=>{
+      return <ShopcateContentsItem key={i}>
+        <ShopitemContents
+          shopitemList={list}
+          index={i+1}
+        />
+      </ShopcateContentsItem>
+      })} 
     </div>
   );
 }
