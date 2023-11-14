cc700b82fe4ef9e314108a3f584354f524de2aee
DESKTOP-8AG16VG\TJ <iamonlyone.u1@gmail.com>
2023-11-14 16:04:26 +0900

    2023-11-14 헤더 병합 테스트

--- client/src/components/main/Main.jsx
+++ client/src/components/main/Main.jsx
@@ -3,19 +3,13 @@ import Header from "./header/Header";
 import Footer from "./footer/Footer";
 import Contents from "./contents/Contents";
 import Banner from "./banner/Banner";
-<<<<<<< HEAD
 import ShopitemSection from "../../pages/main/shopitem/ShopitemSection";
 // merge하기 전 Main 컴포넌트 지우거나 정리하고 하기
 import MainVisual from "../../pages/main/visual/components/MainVisual";
 import SnsImg from "../../pages/main/snsimg/components/SnsImg";
 import SubtitleMore from './../../pages/main/subtitle_more/Subtitle_more';
 import UserImg from './../../pages/main/userimg/UserImg';
-
-
-
-=======
 import { Outlet } from 'react-router-dom';
->>>>>>> work/하나
 
 export default function Main() {
 
@@ -26,7 +20,6 @@ export default function Main() {
       <Header>
 
       </Header>
-<<<<<<< HEAD
       <Contents>
         <MainVisual />
         <SnsImg />
@@ -34,9 +27,7 @@ export default function Main() {
         <SubtitleMore />
         <UserImg />
       </Contents>
-=======
       <Outlet />
->>>>>>> work/하나
       <Footer>
       </Footer>
     </>
