9eae81664d60b53990a8420a1c3fc4643246780d
GS <ygs@mail.comn>
2023-11-28 18:24:38 +0900

    배송지부터 포인트까지 UI완성 다음은 결제수단부터

--- client/src/pages/orders/OrderFormWrap.jsx
+++ client/src/pages/orders/OrderFormWrap.jsx
@@ -1,17 +1,20 @@
 import { useRef, useState } from "react";
 import { IoMdArrowDropdown } from "react-icons/io";
+import { FaCheck } from "react-icons/fa6";
+import OrdersProductWrap from "./components/OrdersProductWrap";
+
+
 
 export default function OrderFormWrap(){
-  const [display,setDisplay] = useState('order_invisible');
-  const [form, setForm] = useState({"orderer_id":"", "orderer_mail":"", "orderer_phead":"010","orderer_pbody":"","pass":"", "passcheck":"", "orderer_name":"","reciever_name":"","reciever_place":"","reciever_phead":"010","reciever_pbody":""});
+  const [display,setDisplay] = useState({"mailbox":"order_invisible","requestbox":"request_invisible"});
+  const [form, setForm] = useState({"orderer_id":"", "orderer_mail":"", "orderer_phead":"010","orderer_pbody":"","pass":"", "passcheck":"", "orderer_name":"","reciever_name":"","reciever_place":"","reciever_phead":"010","reciever_pbody":"","reciever_address_detail":"","reciever_request":""});
   const [domain, setDomain] = useState(false);
   const [phead,setPhead] = useState('010');
-
+  const [base,setBase] = useState(false);
+  const [length,setLength] = useState(0);
   function isFocus(e){
     const parent = e.target.parentNode
     parent.classList.add('active')
-    const a = JSON.stringify({"orderer_id":"", "orderer_mail":"", "orderer_phead":"010","orderer_pbody":"","pass":"", "passcheck":"", "orderer_name":"","reciever_name":"","reciever_place":"","reciever_phead":"010","reciever_pbody":""})
-    console.log(a);
   }
 
   function isBlur(e){
@@ -21,23 +24,35 @@ export default function OrderFormWrap(){
   const handleChange = (e) => {
     const {name, value} = e.target;
     setForm({...form, [name] : value});
-    console.log(form);
-
+    if(e.target.name === 'reciever_request'){
+      const letters = e.target.value.split("");
+      console.log(letters.length)
+      setLength(letters.length < 51 ? letters.length : 50)
+    }
   }
   const selectChange= (e) => {
     let {name, value} = e.target;
     setForm({...form, [name] : value});
     setDomain(true);
-    console.log(form);
-    if(e.target.value === 'selftype'){
-      setDisplay('')
-      setForm({...form, orderer_mail : ""});
-      setForm({orderer_mail:''})
+    console.log(display);
+    if(e.target.value !== 'selftype' && e.target.name === 'orderer_mail'){
+      setDisplay({...display, mailbox : "order_invisible"})
+
     }
-    else{
-      setDisplay('order_invisible')
+    else if(e.target.value !== 'selftype' && e.target.name === 'reciever_request'){
+      setDisplay({...display, requestbox : "request_invisible"})
     }
+    else if(e.target.value === 'selftype' && e.target.name === 'orderer_mail'){
+      setDisplay({...display,mailbox : ''})
+      setForm({...form, orderer_mail : ''});
+    }
+    else if(e.target.value === 'selftype' && e.target.name === 'reciever_request'){
+      setDisplay({...display, requestbox : ''})
+      setForm({...form, reciever_request : ''});
+    }
+
   }
+
   const selectPhone= (e)=>{
     let {value} = e.target;
     setPhead(value)
@@ -74,7 +89,7 @@ export default function OrderFormWrap(){
                 <span className="orders_form_box_input_contents_box_email_span">@</span>
               </div>
               <div className="orders_form_box_input_contents_box_email_mail">
-                <div className={`orders_form_box_input_contents_box_email_mail_self_input ${display}`}>
+                <div className={`orders_form_box_input_contents_box_email_mail_self_input ${display.mailbox}`}>
                   <input className="orders_form_box_input" name="orderer_mail" value={form.orderer_mail ? form.orderer_mail : ""} maxLength={20} onChange={handleChange} onFocus={isFocus} onBlur={isBlur}/>
                 </div>
                 <div className="orders_form_box_input_contents_box_email_mail_select_box">
@@ -127,6 +142,9 @@ export default function OrderFormWrap(){
       <div className="orders_form_box">
         <div className="orders_form_box_title_wrap">
           <h2 className="orders_form_box_title">배송지</h2>
+          <button type="button" className="orders_form_box_same_btn">
+            위와 동일하게 채우기
+          </button>
         </div>
         <div className="orders_form_box_input_wrap">
           <div className="orders_form_box_input_title">배송지명</div>
@@ -171,6 +189,63 @@ export default function OrderFormWrap(){
             </div>
           </div>
         </div>
+
+        <div className="orders_form_box_input_wrap">
+          <div className="orders_form_box_input_title">주소</div>
+          <div className="orders_form_box_input_contents_wrap">
+            <div className="orders_form_box_input_contents_address_container">
+              <div className="orders_form_box_input_contents_address_number_wrap">
+                <button className="orders_form_box_input_contents_address_search_btn" type="button">주소찾기</button>
+                <div className="orders_form_box_input_contents_address_number_container">
+                  <div className="orders_form_box_input_contents_address_number_box">
+                    <input disabled className="orders_form_box_input_contents_address_number" type="text" />
+                  </div>
+                </div>
+              </div>
+              <div className="orders_form_box_input_contents_address_title_wrap">
+                <div className="orders_form_box_input_contents_address_title_container">
+                  <input type="text" disabled className="orders_form_box_input_contents_address_title"/>
+                </div>
+              </div>
+              <div className="orders_form_box_input_contents_address_detail_wrap">
+                <input placeholder="상세주소 입력" name="reciever_address_detail" type="text" className="orders_form_box_input_contents_address_detail" value={form.reciever_address_detail ? form.reciever_address_detail : ""} maxLength={20} onChange={handleChange} onFocus={isFocus} onBlur={isBlur}/>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="orders_form_box_checkbox_wrap">
+          <label className="orders_form_box_checkbox_label">
+            <div className={`orders_form_box_checkbox_container ${base ? 'checkbox_active' : ''}`}>
+              <div className={`orders_form_box_checkbox_box ${base ? 'checkbox_active' : ''}`}>
+                  <FaCheck className="orders_form_box_checkbox_span"/>
+              </div>
+              <input className="orders_form_box_checkbox" type="checkbox" checked={base} onChange={(e)=>setBase(e.target.checked)}/>
+            </div>
+          </label>
+          <span className="orders_form_box_checkbox_label_span">기본 배송지로 저장</span>
+        </div>
+        <div className="orders_form_box_delivery_request_wrap">
+          <div className={`orders_form_box_delivery_request_custom_container ${display.requestbox}`}>
+            <div className="orders_form_box_delivery_request_custom_container_input_box">
+            <input placeholder="배송 요청사항을 입력해주세요." name="reciever_request" type="text" className="orders_form_box_input_contents_self_delivery_request" value={form.reciever_request ? form.reciever_request : ""} maxLength={50} onChange={handleChange} onFocus={isFocus} onBlur={isBlur}/>
+            </div>
+            <div className="orders_form_box_delivery_request_custom_container_span_box">
+              <span className="orders_form_box_delivery_request_custom_container_span_left"></span>
+              <span className="orders_form_box_delivery_request_custom_container_span_right">{length}/50</span>
+            </div>
+          </div>
+          <div className="orders_form_box_delivery_request_container">
+            <span className="orders_form_box_delivery_request_span"><IoMdArrowDropdown/></span>
+            <select className="orders_form_box_delivery_request" name="reciever_request" id="reciever_request" onChange={selectChange}>
+              <option value="">배송시 요청사항을 선택해주세요</option>
+              <option value="부재시 문앞에 놓아주세요">부재시 문앞에 놓아주세요</option>
+              <option value="배송전에 미리 연락주세요">배송전에 미리 연락주세요</option>
+              <option value="부재시 경비실에 맡겨주세요">부재시 경비실에 맡겨주세요</option>
+              <option value="부재시 전화주시거나 문자 남겨 주세요">부재시 전화주시거나 문자 남겨 주세요</option>
+              <option value="selftype">직접 입력</option>
+            </select>
+          </div>
+        </div>
       </div>
 
 
@@ -178,16 +253,30 @@ export default function OrderFormWrap(){
       <div className="orders_form_box">
         <div className="orders_form_box_title_wrap">
           <h2 className="orders_form_box_title">주문상품</h2>
+          <p className="orders_form_box_title_product_count">
+            <span className="orders_form_box_title_product_count_span">
+              3건
+            </span>
+          </p>
         </div>
-        <div className="orders_form_box_input_wrap">
-        </div>
+        <OrdersProductWrap
+        ts={'ohouseDelivery'}
+        />
+        <OrdersProductWrap
+        ts={'todayDelivery'}
+        />
+        {/* 오늘출발 시 시간제한도 출력해야함 */}
+        <OrdersProductWrap
+        ts={'furnitureDelivery'}
+        />
       </div>
 
       <div className="orders_form_box">
         <div className="orders_form_box_title_wrap">
           <h2 className="orders_form_box_title">장바구니 쿠폰</h2>
-        </div>
-        <div className="orders_form_box_input_wrap">
+          <p className="orders_form_box_title_cart_coupon">
+            사용 가능한 쿠폰이 없어요.
+          </p>
         </div>
       </div>
 
@@ -195,7 +284,19 @@ export default function OrderFormWrap(){
         <div className="orders_form_box_title_wrap">
           <h2 className="orders_form_box_title">포인트</h2>
         </div>
-        <div className="orders_form_box_input_wrap">
+        <div className="orders_form_box_point_wrap">
+          <div className="orders_form_box_point_container">
+            <div className="orders_form_box_point_box">
+              <input placeholder="0" type="tel" max="100000000" className="orders_form_box_point" value="0" disabled={true}/>
+            </div>
+            <button disabled type="button" className="orders_form_box_point_btn">
+              전액 사용
+            </button>
+          </div>
+          <div className="orders_form_box_point_usable">
+            사용 가능 포인트
+            <span className="orders_form_box_point_usable_span"> 0 P</span>
+          </div>
         </div>
       </div>
 
@@ -203,8 +304,7 @@ export default function OrderFormWrap(){
         <div className="orders_form_box_title_wrap">
           <h2 className="orders_form_box_title">결제수단</h2>
         </div>
-        <div className="orders_form_box_input_wrap">
-        </div>
+        
       </div>
 
     </div>
