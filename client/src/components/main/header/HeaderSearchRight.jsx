213401265f3fe9170cfa8f7a7410aeb6b8df4a1a
HN <iamonlyone.u1@gmail.com>
2023-12-01 18:37:13 +0900

    Style : header - HeaderSearchRight.jxs

--- client/src/components/main/header/HeaderSearchRight.jsx
+++ client/src/components/main/header/HeaderSearchRight.jsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import { PiBellLight, PiBookmarkSimpleLight, PiShoppingCartLight } from "react-icons/pi";
 import { Link } from "react-router-dom";
 import HeaderProfile from "./HeaderProfile";
-import { TfiSearch } from "react-icons/tfi";
+import { getUser } from './../../../pages/utill/sessionStorage';
+import { TfiSearch } from 'react-icons/tfi';
 
 export default function HeaderSearchRight() {
+  const userInfo = getUser();
   const [showProfile, setShowProfile] = useState("header_nav_popup_profile");
   const toggleProfileMenu = (e) => {
     if (showProfile === "header_nav_popup_profile") {
@@ -13,12 +15,12 @@ export default function HeaderSearchRight() {
       setShowProfile("header_nav_popup_profile")
     }
   }
-
   return (
     <>
-      {/* 로그인 전 메뉴 */}
-      {/* <div className="header_logo_searchBox">
-        <img className="header_logo_search_img" src="images/headers/search.png" alt="이미지1" />
+      {!userInfo ?(
+      <>
+      <div className="header_logo_searchBox">
+        <img className="header_logo_search_img" src="/images/headers/search.png" alt="이미지1" />
         <input className="header_logo_search" type="text" placeholder="통합검색" name="header_logo_search" />
       </div>
       <div className="header_logo_right">
@@ -26,9 +28,10 @@ export default function HeaderSearchRight() {
         <Link to="/login" className="header_logo_menu">로그인</Link>
         <Link to="/normalUsers/new" className="header_logo_menu">회원가입</Link>
         <Link to="/customer_center" className="header_logo_menu">고객센터</Link>
-      </div>  */}
-
-      {/* 로그인 후 메뉴 */}
+      </div>
+      </>
+      ):(
+      <>
       <div className="header_logo_searchBox_loginver">
         {/* <img className="header_logo_search_img" src="/images/headers/search.png" alt="검색창 돋보기" />  */}
         <TfiSearch size="20" color="828c94" />
@@ -47,7 +50,9 @@ export default function HeaderSearchRight() {
         <div className={showProfile} >
           <HeaderProfile />
         </div>
-      </div>  
+      </div>
+      </>
+      )}
     </>
   );
 }
