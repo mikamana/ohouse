3dfe6536c25d61b4fa792be1691da266d05aa0fe
GS <ygs@mail.comn>
2023-12-04 14:49:19 +0900

    DOCS : cartRepository의 getCart 함수 sql 쿼리 수정

--- server/repository/cartRepository.js
+++ server/repository/cartRepository.js
@@ -9,7 +9,9 @@ export async function createCart(pid,id,qty){
 }
 
 export async function getCart(mid){
-  const sql = "select cart_id, pid, mid,qty, cdate from oh_cart where mid = ?"
+  const sql = `select cart_id, op.pid, om.mid ,qty ,cdate, ifnull(format(round(op.price_origin - (op.price_origin * op.price_sale / 100),-2),0),format(op.price_origin,0)) sale_price, ifnull(format((round(op.price_origin - (op.price_origin * op.price_sale / 100),-2) * oc.qty),0),round(op.price_origin - (op.price_origin * op.price_sale / 100),-2)) total_price 
+  , op.category_id, op.product_image,op.brand_name,op.delivery_type,op.product_name,op.tag_free, cnt
+  from oh_cart oc, oh_product op, oh_member om,(select count(*) as cnt from oh_cart where mid = ?) cart where oc.pid = op.pid and oc.mid = om.mid and oc.mid = ?;`
   return db.execute(sql,[mid])
   .then((result)=>result[0])
 }
