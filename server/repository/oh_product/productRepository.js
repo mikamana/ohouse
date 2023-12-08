495b827abe387932bb36fee355b9760c2d6d261e
GS <ygs@mail.comn>
2023-12-08 11:22:09 +0900

    FEAT : CartSection 가격연동 모두 완료

--- server/repository/oh_product/productRepository.js
+++ server/repository/oh_product/productRepository.js
@@ -1,26 +1,26 @@
 import {db} from '../../data/database.js'
 
 export async function getTodayDeal(){
-  const sql = 'select pid,category_id,product_image,brand_name,product_name,rating_avg,rating_review,price_sale,price_origin,ifnull(format(round(price_origin - (price_origin * price_sale / 100),-2),0),format(price_origin,0)) sale_price,tag_free,coupon_percent,pdate,delivery_type from oh_product where product_name like "%오늘의딜%" limit 11'
+  const sql = 'select pid,category_id,product_image,brand_name,product_name,rating_avg,rating_review,price_sale,price_origin,ifnull(round(price_origin - (price_origin * price_sale / 100),-2),price_origin) sale_price,tag_free,coupon_percent,pdate,delivery_type from oh_product where product_name like "%오늘의딜%" limit 11'
   return db
   .execute(sql)
   .then(result => result[0])
 }
 export async function getPopular(){
-  const sql = 'select pid,category_id,product_image,brand_name,product_name,rating_avg,rating_review,price_sale,price_origin,ifnull(format(round(price_origin - (price_origin * price_sale / 100),-2),0),format(price_origin,0)) sale_price,tag_free,coupon_percent,pdate,delivery_type from oh_product order by rating_review desc'
+  const sql = 'select pid,category_id,product_image,brand_name,product_name,rating_avg,rating_review,price_sale,price_origin,ifnull(round(price_origin - (price_origin * price_sale / 100),-2),price_origin) sale_price,tag_free,coupon_percent,pdate,delivery_type from oh_product order by rating_review desc'
   return db
   .execute(sql)
   .then(result => result[0])
 }
 export async function getBestitem(){
-  const sql = 'select pid,category_id,product_image,brand_name,product_name,rating_avg,rating_review,price_sale,price_origin,ifnull(format(round(price_origin - (price_origin * price_sale / 100),-2),0),format(price_origin,0)) sale_price,tag_free,coupon_percent,pdate,delivery_type from oh_product order by rating_avg desc limit 1,3'
+  const sql = 'select pid,category_id,product_image,brand_name,product_name,rating_avg,rating_review,price_sale,price_origin,ifnull(round(price_origin - (price_origin * price_sale / 100),-2),price_origin) sale_price,tag_free,coupon_percent,pdate,delivery_type from oh_product order by rating_avg desc limit 1,3'
   return db
   .execute(sql)
   .then(result => result[0])
 }
 
 export async function getCategoryList(category_id){
-  const sql = 'select pid,category_id,product_image,brand_name,product_name,rating_avg,rating_review,price_sale,price_origin,ifnull(format(round(price_origin - (price_origin * price_sale / 100),-2),0),format(price_origin,0)) sale_price,tag_free,coupon_percent,pdate,delivery_type from oh_product where category_id = ? order by rating_avg desc limit 1,3'
+  const sql = 'select pid,category_id,product_image,brand_name,product_name,rating_avg,rating_review,price_sale,price_origin,ifnull(round(price_origin - (price_origin * price_sale / 100),-2),price_origin) sale_price,tag_free,coupon_percent,pdate,delivery_type from oh_product where category_id = ? order by rating_avg desc limit 1,3'
   return db
   .execute(sql,[category_id])
   .then(result => result[0])
